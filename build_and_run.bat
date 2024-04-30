6a8805f2bff011578413ac0f0e552939b16b7cfa
SerpentDash <d.k500555@gmail.com>
2024-04-30 16:46:11 +0200

    Automate building process

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,4 +1,5 @@
 @echo off
-call cmd /c flutter build apk --split-per-abi --obfuscate --split-debug-info=.\build\debug-info
-call cmd /c adb install .\build\app\outputs\flutter-apk\app-arm64-v8a-release.apk
+call cmd /c flutter build apk --split-per-abi --obfuscate --split-debug-info=.\build\debug-info || exit /b
+for /f "delims=" %%a in ('dir /b /s .\build\app\outputs\apk\release\*arm64-v8a*.apk 2^>nul') do call cmd /c adb install "%%a" || exit /b
 call cmd /c adb shell am start -n 'pl.serpentdash.shinden_mobile/.MainActivity'
+if "%~1" == "-show" start explorer.exe .\build\app\outputs\apk\release\
